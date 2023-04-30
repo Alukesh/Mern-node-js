267aa32b9267b46a2024c4785e1bee8fba4b8468
Alukesh <akenzhebaev422@gmail.com>
2023-04-30 19:26:43 +0600

    fix corse 3

--- index.js
+++ index.js
@@ -18,25 +18,8 @@ const cors = require("cors");
 app.use(express.json())
 app.use('/uploads', express.static('uploads'))
 
-app.use(cors({
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-}))
-//   app.options('*', cors());
-app.use((req, res, next) => {
-    //allow access from every, elminate CORS
-    res.header('Access-Control-Allow-Origin', '*');
-    res.removeHeader('x-powered-by');
-    //set the allowed HTTP methods to be requested
-    res.header(`Access-Control-Allow-Methods`, `GET,PATCH,POST,DELETE`);
-    //headers clients can use in their requests
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    //allow request to continue and be handled by routes
-    next();
-});
+app.use(cors())
+  app.options('*', cors());
 
 const storage = multer.diskStorage({
     destination: (_, __, callback) => {
